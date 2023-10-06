2e2a2a501aee30f572f8b9591d5cccfaedf9487e
mens1s <ahmet_yigit_02@hotmail.com>
2023-10-06 21:06:44 +0300

    ecommerce project added

---
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -0,0 +1,85 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Luv2ShopFormService } from '../../services/luv2-shop-form.service';
+
+@Component({
+  selector: 'app-checkout',
+  templateUrl: './checkout.component.html',
+  styleUrls: ['./checkout.component.css']
+})
+export class CheckoutComponent implements OnInit {
+
+  checkoutFromGroup: FormGroup;
+  
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+
+  creditCardYears: number[] = [];
+  creditCardMonths: number[] = [];
+
+  constructor(private formBuilder: FormBuilder,
+              private luv2ShopFormService: Luv2ShopFormService) { }
+
+  ngOnInit(): void {
+    this.checkoutFromGroup = this.formBuilder.group({
+      customer: this.formBuilder.group({
+        firstName: [''],
+        lastName: [''],
+        email: ['']
+      }),
+      shippingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: ['']
+      }),
+      billingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: ['']
+      }),
+      creditCard: this.formBuilder.group({
+        cardType: [''],
+        nameOnCard: [''],
+        cardNumber: [''],
+        securityCode: [''],
+        expirationMonth: [''],
+        expirationYear: ['']
+      }),
+    });
+
+    const startMonth: number = new Date().getMonth() + 1;
+
+    this.luv2ShopFormService.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        this.creditCardMonths = data;
+      }
+    )
+
+    this.luv2ShopFormService.getCreditCardYears().subscribe(
+      data => {
+        this.creditCardYears = data;
+      }
+    )
+  }
+
+  onSubmit(){
+    console.log("Handling the submit button!");
+    console.log(this.checkoutFromGroup.get('customer').value);
+  }
+
+  copyShippingAddressToBillingAddress(event){
+    if(event.target.checked){
+      this.checkoutFromGroup.controls.billingAddress
+        .setValue(this.checkoutFromGroup.controls.shippingAddress.value);
+    }
+    else{
+      this.checkoutFromGroup.controls.billingAddress.reset();
+    }
+
+  }
+
+}
