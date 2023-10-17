cf74d79d6cc9c1b9a2e0a08c9b652269ee0f08fb
mens1s <ahmet_yigit_02@hotmail.com>
2023-10-17 23:25:15 +0300

    order db and models created

--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/entity/OrderItem.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/entity/OrderItem.java
@@ -1,2 +1,34 @@
-package com.luv2code.ecommerce.entity;public class OrderItem {
+package com.luv2code.ecommerce.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="order_item")
+@Getter
+@Setter
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = javax.persistence.GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "unit_price")
+    private String unitPrice;
+
+    @Column(name = "quantity")
+    private String quantity;
+
+    @Column(name = "product_id")
+    private String productId;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
 }
